85c13519b5890770deb19de4336865bdbb4faae4
Natgonza <natgonza99@gmail.com>
2025-10-01 15:53:23 -0600

    Added comments to Parser.py as a description of what it does and what it is looking for.

--- Pipeline/Parser.py
+++ Pipeline/Parser.py
@@ -1,1 +1,10 @@
-#10/1/2025
+"""
+ This is a parser that takes a .mzML file from the mass spectrometers and parsers through
+ looking for "" and "" and storing them inside of the "" file for storage. 
+"""
+
+"""
+The parser is looking for:
+ MS Level: 2
+ Value: 
+"""
